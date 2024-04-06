7dd4f643229aac9877450398aba7ed9d54ed9854
Jammonjones <wjssaan@gmail.com>
2024-04-06 17:47:41 -0600

    very simple react native app build and working on my android

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  { Task }  from './components/Task.jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    {/* todays tasks */}
+    <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Today's Tasks</Text>
+      <View style={styles.items}>
+        {/* This is where the tasks will go */}
+        <Task text={"Task 1"}></Task>
+        <Task text={"Task 2"}></Task>
+      </View>
+    </View>
     </View>
   );
 }
@@ -13,8 +22,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#BBA2FF'
   },
+  // Looks like this is where you add your styles instead of
+  // a css file...
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  items: {}
 });
---
+++ components/Task.jsx
@@ -0,0 +1,18 @@
+import react from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+})
+
+import React from 'react'
+
+export const Task = () => {
+  return (
+    <View>
+      <Text>YOOO</Text>
+    </View>
+  )
+}
+
+
